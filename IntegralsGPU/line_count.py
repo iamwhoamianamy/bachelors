281c4aa4b8786b9667d3bc14f2a6961fb534ec6b
zadolbali10 <anon.yu@mail.ru>
2021-11-08 17:19:47 +0700

    Created script for counting lines in project

---
+++ IntegralsGPU/line_count.py
@@ -0,0 +1,15 @@
+from os import listdir
+from os.path import isfile, join
+import re
+
+mypath = "./"
+
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+lines_count = 0
+
+for file in onlyfiles:
+   if ".cu" in file or ".cuh" in file or ".cpp" in file or ".h" in file:
+      lines_count += sum(1 for line in open(file))
+
+print(lines_count)
