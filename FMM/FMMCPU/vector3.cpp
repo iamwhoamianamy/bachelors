05e8014092201094e014c6812af1c4dca1a75c86
zadolbali10 <anon.yu@mail.ru>
2022-02-21 19:06:22 +0700

    Some vector3 class improvements

---
+++ FMM/FMMCPU/vector3.cpp
@@ -0,0 +1,217 @@
+#include "vector3.hpp"
+#include <exception>
+
+Vector3::Vector3(const Vector3& vector)
+{
+   x = vector.x;
+   y = vector.y;
+   z = vector.z;
+}
+
+Vector3::Vector3(Vector3&& vector) noexcept
+{
+   x = std::move(vector.x);
+   y = std::move(vector.y);
+   z = std::move(vector.z);
+}
+
+Vector3::Vector3(real x, real y, real z)
+   : x(x), y(y), z(z) {}
+
+real& Vector3::operator[](int i)
+{
+   switch(i)
+   {
+      case 0: return x;
+      case 1: return y;
+      case 2: return z;
+      default: throw std::exception();
+   }
+}
+
+Vector3& Vector3::operator=(Vector3&& vector) noexcept
+{
+   x = std::move(vector.x);
+   y = std::move(vector.y);
+   z = std::move(vector.z);
+
+   return *this;
+}
+
+Vector3& Vector3::operator=(const Vector3& vector)
+{
+   x = vector.x;
+   y = vector.y;
+   z = vector.z;
+
+   return *this;
+}
+
+Vector3 Vector3::operator +(const Vector3& rhs) const
+{
+   Vector3 res;
+   res.x = this->x + rhs.x;
+   res.y = this->y + rhs.y;
+   res.z = this->z + rhs.z;
+
+   return res;
+}
+
+Vector3 Vector3::operator -(const Vector3& rhs) const
+{
+   Vector3 res;
+   res.x = this->x - rhs.x;
+   res.y = this->y - rhs.y;
+   res.z = this->z - rhs.z;
+
+   return res;
+}
+
+Vector3 Vector3::operator *(const real fac) const
+{
+   Vector3 res;
+   res.x = this->x * fac;
+   res.y = this->y * fac;
+   res.z = this->z * fac;
+
+   return res;
+}
+
+Vector3 Vector3::operator /(const real fac) const
+{
+   Vector3 res;
+   res.x = this->x / fac;
+   res.y = this->y / fac;
+   res.z = this->z / fac;
+
+   return res;
+}
+
+Vector3& Vector3::operator +=(const Vector3& rhs)
+{
+   this->x += rhs.x;
+   this->y += rhs.y;
+   this->z += rhs.z;
+
+   return *this;
+}
+
+Vector3& Vector3::operator -=(const Vector3& rhs)
+{
+   this->x -= rhs.x;
+   this->y -= rhs.y;
+   this->z -= rhs.z;
+
+   return *this;
+}
+
+Vector3& Vector3::operator *=(const real fac)
+{
+   this->x *= fac;
+   this->y *= fac;
+   this->z *= fac;
+
+   return *this;
+}
+
+Vector3& Vector3::operator /=(const real fac)
+{
+   if(fac != 0)
+   {
+      this->x /= fac;
+      this->y /= fac;
+      this->z /= fac;
+   }
+   else
+   {
+      this->x = 0;
+      this->y = 0;
+      this->z = 0;
+   }
+
+   return *this;
+}
+
+real Vector3::LengthSquared() const
+{
+   return this->x * this->x +
+      this->y * this->y +
+      this->z * this->z;
+}
+
+real Vector3::Length() const
+{
+   return sqrt(this->x * this->x +
+               this->y * this->y +
+               this->z * this->z);
+}
+
+Vector3 Vector3::Normalized() const
+{
+   Vector3 res;
+   real length = Length();
+
+   if(length)
+   {
+      res.x = this->x / length;
+      res.y = this->y / length;
+      res.z = this->z / length;
+   }
+
+   return res;
+}
+
+void Vector3::Normalize()
+{
+   real length = Length();
+
+   if(length != 0)
+   {
+      this->x /= length;
+      this->y /= length;
+      this->z /= length;
+   }
+}
+
+void Vector3::Limit(const real limitLength)
+{
+   real length = Length();
+
+   if(length != 0 && length > limitLength)
+   {
+      this->x = this->x / length * limitLength;
+      this->y = this->y / length * limitLength;
+      this->z = this->z / length * limitLength;
+   }
+}
+
+void Vector3::SetLength(const real newLength)
+{
+   Normalize();
+   this->x *= newLength;
+   this->y *= newLength;
+   this->z *= newLength;
+}
+
+Vector3 Vector3::Perp() const
+{
+   return Vector3(-y, x);
+}
+
+Vector3 Vector3::Direction(const Vector3& from, const Vector3& to)
+{
+   Vector3 res = to - from;
+   return res.Normalized();
+}
+
+real Vector3::DistanceSquared(const Vector3& vec1, const Vector3& vec2)
+{
+   return (vec1 - vec2).LengthSquared();
+}
+
+Vector3 Vector3::LeadingCos()
+{
+   real l = Length();
+
+   return Vector3(x / l, y / l, z / l);
+}
---
+++ FMM/FMMCPU/vector3.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include "real.hpp"
+
+class Vector3
+{
+public:
+   real x;
+   real y;
+   real z;
+
+   Vector3(const Vector3& vector);
+   Vector3(Vector3&& vector) noexcept;
+   Vector3(real x = 0, real y = 0, real z = 0);
+
+   real& operator[](int i);
+
+   Vector3& operator=(Vector3&& vector) noexcept;
+   Vector3& operator=(const Vector3& vector);
+
+   Vector3 operator +(const Vector3& rhs) const;
+   Vector3 operator -(const Vector3& rhs) const;
+   Vector3 operator *(const real fac) const;
+   Vector3 operator /(const real fac) const;
+
+   Vector3& operator +=(const Vector3& rhs);
+   Vector3& operator -=(const Vector3& rhs);
+   Vector3& operator *=(const real fac);
+   Vector3& operator /=(const real fac);
+
+   real LengthSquared() const;
+   real Length() const;
+   Vector3 Normalized() const;
+   Vector3 Perp() const;
+
+   void Normalize();
+   void Limit(const real limitLength);
+   void SetLength(const real newLength);
+   Vector3 LeadingCos();
+
+   static Vector3 Direction(const Vector3& from, const Vector3& to);
+   static real DistanceSquared(const Vector3& vec1, const Vector3& vec2);
+};
