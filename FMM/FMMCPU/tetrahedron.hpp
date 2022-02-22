a7b0100178f9f152be1a0bc0aca2de9d1a1158dc
zadolbali10 <anon.yu@mail.ru>
2022-02-22 22:49:52 +0700

    Added torus fields

--- FMM/FMMCPU/tetrahedron.hpp
+++ FMM/FMMCPU/tetrahedron.hpp
@@ -4,11 +4,10 @@
 
 class Tetrahedron
 {
-private:
-   std::vector<Vector3> points;
 public:
    Tetrahedron();
    Tetrahedron(Vector3& a, Vector3& b, Vector3& c, Vector3& d);
+   std::vector<Vector3> points;
 
    Vector3& a();
    Vector3& b();
--- FMM/FMMCPU/torus.hpp
+++ FMM/FMMCPU/torus.hpp
@@ -1,17 +1,32 @@
 #pragma once
 #include <vector>
 #include "vector3.hpp"
+#include "tetrahedron.hpp"
 #include "hexahedron.hpp"
 
 class Torus
 {
+private:
+   const double radius;
+   const double sectionWidth;
+   const int onLengthStepCount;
+   const int onWidthStepCount;
+   const int onHeightStepCount;
+   std::vector<Tetrahedron> tetrahedra;
 public:
    Torus(const double radius,
          const double sectionWidth,
          const int onLengthStepCount,
          const int onWidthStepCount,
          const int onHeightStepCount);
+   double innerRadius() const;
+   double outerRadius() const;
+   double bottom() const;
+   double top() const;
+   double stepAngle() const;
 
-   std::vector<Hexahedron> hexahedrons;
+private:
+   void buildHexahedra(std::vector<Hexahedron>& hexahedra);
+   void buildTetrahedra(std::vector<Hexahedron>& hexahedra);
 };
 
