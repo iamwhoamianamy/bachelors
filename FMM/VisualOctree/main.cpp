eab7b7e961bb0f5456488510f0c1b24038f2a2fa
zadolbali10 <anon.yu@mail.ru>
2022-03-03 12:19:28 +0700

    Created poject for octree visualisation

---
+++ FMM/VisualOctree/main.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "glut_functions.hpp"
+#include "window.hpp"
+
+int main(int argc, char** argv)
+{
+   glut_functions::window = new Window(argc, argv, 10, 800, 300, "Octree");
+}
