60326b4f0ab0bff560431f2dcb62b8fe2b36a337
zadolbali10 <anon.yu@mail.ru>
2022-06-07 21:20:53 +0700

    Removed redundant files

--- FMM/FMMGPU/tetrahedron_transformer.cpp
+++
@@ -1,9 +0,0 @@
-#include "tetrahedron_transformer.hpp"
-
-Vector3 TetrahedronTransformer::pointFromBasisQuadrature(const Tetrahedron& tetr, const Vector3 basisQuadrature)
-{
-   return tetr.a() + 
-      (tetr.b() - tetr.a()) * basisQuadrature.x +
-      (tetr.c() - tetr.a()) * basisQuadrature.y + 
-      (tetr.d() - tetr.a()) * basisQuadrature.z;
-}
